e6f7768881650ae23bcd73d9e74cac52672a50c6
Marcela Paíno <marcepaino@gmail.com>
2016-07-05 19:47:04 -0300

    Add scrabble exercise

---
+++ scrabble-score/src/scrabble_score.clj
@@ -0,0 +1,16 @@
+(ns scrabble-score
+   (:require [clojure.string :as str]))
+
+(def points {"d" 2, "g" 2,
+             "b" 3, "c" 3, "m" 3, "p" 3
+             "f" 4, "h" 4, "v" 4, "w" 4, "y" 4
+             "k" 5,
+             "j" 8, "x" 8
+             "q" 10, "z" 10})
+
+(defn score-letter [letter]
+  (get points (str/lower-case letter) 1))
+
+(defn score-word [word]
+    (reduce + (map #(score-letter (str %)) word)))
+
