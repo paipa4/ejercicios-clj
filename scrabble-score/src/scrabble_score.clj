1a8aa91baed2e292265092e2d5ac173278e7c6fb
Marcela Paíno <marcepaino@gmail.com>
2016-07-05 20:04:14 -0300

    Change scrabble solution

--- scrabble-score/src/scrabble_score.clj
+++ scrabble-score/src/scrabble_score.clj
@@ -1,16 +1,18 @@
 (ns scrabble-score
    (:require [clojure.string :as str]))
 
-(def points {"d" 2, "g" 2,
-             "b" 3, "c" 3, "m" 3, "p" 3
-             "f" 4, "h" 4, "v" 4, "w" 4, "y" 4
-             "k" 5,
-             "j" 8, "x" 8
-             "q" 10, "z" 10})
+(def points {\d 2, \g 2
+             \b 3, \c 3, \m 3, \p 3
+             \f 4, \h 4, \v 4, \w 4, \y 4
+             \k 5
+             \j 8, \x 8
+             \q 10, \z 10})
 
 (defn score-letter [letter]
-  (get points (str/lower-case letter) 1))
+  (if (string? letter)
+     (score-letter (first (str/lower-case letter)))
+     (get points letter 1)))
 
 (defn score-word [word]
-    (reduce + (map #(score-letter (str %)) word)))
+    (reduce + (map score-letter (str/lower-case word))))
 
