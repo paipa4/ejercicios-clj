b8051eca5b5761f3dc289d544cdb4035c6bc2e7b
Marcela Paíno <marcepaino@gmail.com>
2016-07-29 15:00:28 -0300

    Add modification

--- accumulate/src/accumulate.clj
+++ accumulate/src/accumulate.clj
@@ -1,10 +1,10 @@
 (ns accumulate)
 
-(defn accumulate [function collection]
-  (let [fun function]
+(defn accumulate [fun collection]
   (loop [coll collection
          result [] ]
     (if (empty? coll)
       result
       (recur (rest coll)
-             (conj result (fun (first coll))))))))
+             (conj result (fun (first coll)))))))
+
